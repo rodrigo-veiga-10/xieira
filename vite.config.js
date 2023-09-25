d3671d6aa3ccf0b6e03f16f546739055741e2faf
rodrigo-veiga-10 <rodriveiga10@gmail.com>
2023-09-25 19:21:15 +0100

    text gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
 })
 
